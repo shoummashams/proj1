093a78d1d82fa8e214b981952c833c88edab6043
Shoumma Shams <shoumma@gmail.com>
2017-10-27 10:01:38 -0700

    Fix for Ruby error and initial Part 2
    Part 2 not functional

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,3 +1,8 @@
 class PokemonController < ApplicationController
-
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.trainer_id = current_user
+		@pokemon.save
+		redirect_to "home#index"
+	end
 end
